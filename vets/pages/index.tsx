22d68708cc17ddb83a9ea25071ef4f89972da748
alpha@DESKTOP-6F6AEHR <blkcdev@gmail.com>
2023-02-20 17:29:20 -0800

    fixed bugs

--- vets/pages/index.tsx
+++ vets/pages/index.tsx
@@ -50,7 +50,7 @@ export default function Home() {
 </Box>
       {/* <Spoiler maxHeight={120} showLabel="Show more" hideLabel="Hide">
       {/* Content here */} 
-    {/* </Spoiler> */}//deploy-vets-2023feb20-v8
+    {/* </Spoiler> */}
       
 
       {/* <Container size="xs">
